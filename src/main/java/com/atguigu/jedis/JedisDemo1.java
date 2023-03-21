3b6189fec7ec297c419e0c8464fa041996eb6894
jiangyang <591277686@qq.com>
2023-03-21 23:31:24 +0800

    test2

--- src/main/java/com/atguigu/jedis/JedisDemo1.java
+++ src/main/java/com/atguigu/jedis/JedisDemo1.java
@@ -1,4 +1,27 @@
 package com.atguigu.jedis;
 
+import org.junit.Test;
+import redis.clients.jedis.Jedis;
+
+import java.util.Set;
+
 public class JedisDemo1 {
+    public static void main(String[] args) {
+        Jedis jedis=new Jedis("saic01.top",6379);
+        String value =jedis.ping() ;
+        System.out.println(value);
+
+    }
+
+ @Test
+    public void demo1(){
+     Jedis jedis=new Jedis("saic01.top",6379);
+     Set<String> keys = jedis.keys("*");
+     for(String key:keys){
+         System.out.println(key);
+     }
+
+
+ }
+
 }
